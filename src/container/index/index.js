33b99fd2144e52b7752aa6b467f7253376e0caac
Serik1205 <barba_sl@i.ua>
2023-09-27 00:24:19 +0300

    init

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,4 @@
-import '../../script/test'
-
+import { message } from "../../script/test";
+import MyName from "../../script/test";
 console.log('container')
+console.log(MyName);
--- src/script/test.js
+++ src/script/test.js
@@ -1,1 +1,9 @@
-console.log('script')
+//console.log('script')
+export const message = 'Default text';
+
+const message2 = () => { }
+
+function Message3() { }
+const message3 = 'script';
+export { message3, Message3, message2 }
+export default message
